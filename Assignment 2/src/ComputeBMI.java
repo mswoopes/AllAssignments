a21e1099f4f5af65eca994e28bac6b10bf041e5a
mswoopes <mswoopes@mail.stmarytx.edu>
2015-02-11 12:07:34 -0600

    Calculation of Miles Per Gallon & Calculation of Body Mass Index

---
+++ Assignment 2/src/ComputeBMI.java
@@ -0,0 +1,37 @@
+import javax.swing.JOptionPane;
+
+public class ComputeBMI
+{
+   public static void main(String[] args)
+   {
+      String inputString;	// For reading input
+      int weight1;          // Weight in pounds
+      double weight2;		// Weight in kilograms
+      int height1;			// Height in inches
+      double height2;		// Height in meters
+      double height3;		// Height for calculation
+      double bmi;			// The body mass index
+      
+      inputString =
+        JOptionPane.showInputDialog("Enter your weight " + "in pounds. ");
+
+      weight1 = Integer.parseInt(inputString);
+
+      inputString =
+    	JOptionPane.showInputDialog("Enter your height " + "in inches. ");
+
+      height1 = Integer.parseInt(inputString);
+    	     
+      weight2 = weight1 * 0.45359237;
+    		  
+      height2 = height1 * 0.0254;
+      
+      height3 = height2 * height2;
+      
+      bmi = weight2 / height2;
+            
+      JOptionPane.showMessageDialog(null, "Your Body Mass Index is " + bmi);
+
+      System.exit(0);
+   }
+}
---
+++ Assignment 2/src/MilesPerGallon.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MilesPerGallon
+{
+   public static void main(String[] args)
+   {
+      int miles;           // Miles Driven
+      double gallons;      // Gallons used
+      double mpg;		   // miles-per-gallon
+      
+      Scanner keyboard = new Scanner(System.in);
+            
+      System.out.print("How many miles were driven? ");
+      miles = keyboard.nextInt();
+      
+      System.out.print("How many gallons of fuel was used? ");
+      gallons = keyboard.nextDouble();
+      
+      mpg = miles / gallons;
+      
+      System.out.println("The Miles per Gallon is " + mpg);
+   }
+}
